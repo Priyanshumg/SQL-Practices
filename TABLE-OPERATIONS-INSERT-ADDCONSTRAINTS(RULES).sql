18c6874e69fc04f661bc7b362ffd8f1e57bae564
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-02-15 15:47:36 +0530

    [Priyanshu] Add: Added a Default Constraint for TBLPerson(GenderID)

--- TABLE-OPERATIONS-INSERT-ADDCONSTRAINTS(RULES).sql
+++ TABLE-OPERATIONS-INSERT-ADDCONSTRAINTS(RULES).sql
@@ -14,3 +14,11 @@ INSERT INTO TBLPerson (ID, "Name", Email, GenderID) VALUES (3, 'Martin', 'ma@ma.
 INSERT INTO TBLPerson (ID, "Name", Email) VALUES (4, 'Rob', 'r@r.com')
 INSERT INTO TBLPerson (ID, "Name", Email, GenderID) VALUES (5, 'May', 'my@my.com',2)
 INSERT INTO TBLPerson (ID, "Name", Email) VALUES (6, 'Kristy', 'k@k.com')
+
+-- Adding A Default Constraint
+ALTER TABLE TBLPerson
+-- Constraints Define Rules
+ADD CONSTRAINT DK_TBLPerson_GenderID
+-- This Rule Enables if no value is been passed then default would be 3
+-- But, If value has been passed as Null or Anything it will Update as Null
+DEFAULT 3 FOR GenderID
