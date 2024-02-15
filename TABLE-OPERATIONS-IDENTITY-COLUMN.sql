fbe33115cce72449e6c0742852caf0d704cd6fdf
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-02-15 20:59:18 +0530

    [Priyanshu] Add: Added new table to DataBase(TBLIdentityPerson)

---
+++ TABLE-OPERATIONS-IDENTITY-COLUMN.sql
@@ -0,0 +1,8 @@
+-- Creating a new table
+CREATE TABLE TBLIdentityPerson
+(
+-- Declaring PersonId Column as an Identity Column with Seed as 1 and Increment Value as 1
+[PersonID] [INT] IDENTITY(1,1)  Not Null,
+PersonName nvarchar(50)
+)
+
