9895a1446832f3b6358798e8bbd2cdc29e62df99
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-02-15 15:46:47 +0530

    [Priyanshu] Add: Added Elements to TBLPerson

--- TABLE-OPERATIONS-INSERT-ADDCONSTRAINTS(RULES).sql
+++ TABLE-OPERATIONS-INSERT-ADDCONSTRAINTS(RULES).sql
@@ -6,3 +6,11 @@ SELECT * FROM TBLPerson
 INSERT INTO TBLGender (ID, Gender) VALUES (1, 'Male')
 INSERT INTO TBLGender (ID, Gender) VALUES (2, 'Female')
 INSERT INTO TBLGender (ID, Gender) VALUES (3, 'Unknown')
+
+--Inserting Values into TBLPerson
+INSERT INTO TBLPerson (ID, "Name", Email, GenderID) VALUES (1, 'Jade', 'j@j.com', 2)
+INSERT INTO TBLPerson (ID, "Name", Email, GenderID) VALUES (2, 'Mary', 'm@m.com', 3)
+INSERT INTO TBLPerson (ID, "Name", Email, GenderID) VALUES (3, 'Martin', 'ma@ma.com', 1)
+INSERT INTO TBLPerson (ID, "Name", Email) VALUES (4, 'Rob', 'r@r.com')
+INSERT INTO TBLPerson (ID, "Name", Email, GenderID) VALUES (5, 'May', 'my@my.com',2)
+INSERT INTO TBLPerson (ID, "Name", Email) VALUES (6, 'Kristy', 'k@k.com')
