8fd8118d5c819116421dacff5102800d1c6debf1
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-02-15 17:47:31 +0530

    [Priyanshu] Add: Added True Value to check

--- TABLE-CHECK-CONSTRAINT.sql
+++ TABLE-CHECK-CONSTRAINT.sql
@@ -13,3 +13,6 @@ CHECK (Age > 0 AND Age < 150)
 -- Inserting False Value 
 INSERT INTO TBLPerson VALUES (7,'AngelPriya', 'AP@AP.com', 2, 970)
 -- Throwed Error: Check Constraint
+
+INSERT INTO TBLPerson VALUES (7,'AngelPriya', 'AP@AP.com', NULL, 97)
+-- ADDED ELEMENT TO THE TABLE ROW
