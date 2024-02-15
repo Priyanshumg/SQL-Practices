1a44b0fb1ea3fa384a602352ec053190d27839b6
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-02-15 17:46:00 +0530

    [Priyanshu] Add: Added new constraint rule to TBLPerson(CK_TBLPerson_Age

--- TABLE-CHECK-CONSTRAINT.sql
+++ TABLE-CHECK-CONSTRAINT.sql
@@ -4,3 +4,8 @@ SELECT * FROM TBLPerson
 -- Adding Column
 ALTER TABLE TBLPerson
 ADD Age INT
+
+-- Adding New Constrait to TBLPerson
+ALTER TABLE TBLPerson
+ADD CONSTRAINT CK_TBLPerson_Age
+CHECK (Age > 0 AND Age < 150)
