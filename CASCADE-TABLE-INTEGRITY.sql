cf073738b4de49fe8d0ee18cf92c1f9b11ab2268
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-02-15 16:40:04 +0530

    [Priyanshu] Mod: Modified Table Functionality(TBLGender), If Row deleted then FK Setting to setDEFAULT

--- CASCADE-TABLE-INTEGRITY.sql
+++ CASCADE-TABLE-INTEGRITY.sql
@@ -10,3 +10,8 @@ SELECT * FROM TBLPerson
 
 -- Practical Testing Error - Deleting Row2 in TBLGender
 DELETE FROM TBLGender WHERE ID = 2
+-- Above Code throws Error(DELETE Statement Conflict)
+-- As No rule is Assigned to the DELETE ACTION in GENDER
+
+-- We Changed Property of TBLPerson Key When an Element is Deleted, It will SET DEFAULT
+-- Now Lets Run The same OLD Command
