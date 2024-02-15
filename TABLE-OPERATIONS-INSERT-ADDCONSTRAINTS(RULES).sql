9654ae97f2e3c66d3fec818373b756b04aac1f17
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-02-15 15:45:20 +0530

    [Priyanshu] Add: Added Elements to the Table TBLGender

--- TABLE-OPERATIONS-INSERT-ADDCONSTRAINTS(RULES).sql
+++ TABLE-OPERATIONS-INSERT-ADDCONSTRAINTS(RULES).sql
@@ -1,3 +1,8 @@
 -- Display Table SQL
 SELECT * FROM TBLGender
 SELECT * FROM TBLPerson
+
+-- Inserting Values into TBLGender
+INSERT INTO TBLGender (ID, Gender) VALUES (1, 'Male')
+INSERT INTO TBLGender (ID, Gender) VALUES (2, 'Female')
+INSERT INTO TBLGender (ID, Gender) VALUES (3, 'Unknown')
