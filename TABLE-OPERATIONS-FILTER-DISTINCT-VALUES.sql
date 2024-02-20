ac91b68b8e16aab08806c845d04450bba381cbb3
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-02-20 11:13:24 +0530

    [Priyanshu] Add: Added Elements to the Table

--- TABLE-OPERATIONS-FILTER-DISTINCT-VALUES.sql
+++ TABLE-OPERATIONS-FILTER-DISTINCT-VALUES.sql
@@ -14,3 +14,55 @@ CREATE TABLE Customers
 	PostalCode int Not Null,
 	Country nvarchar(50) Not Null,
 )
+
+-- Data Entry Starts from Here -- 
+INSERT INTO Customers Values
+(
+	'Alfreds Futterkiste',
+	'Maria Anders',
+	'Obere Str. 57',
+	'Berlin',
+	12209,
+	'Germany'
+)
+
+INSERT INTO Customers Values
+(
+	'Ana Trujillo Emparedados y helados',
+	'Ana Trujillo',
+	'Avda. de la Constitución 2222',
+	'México D.F.',
+	05021,
+	'Mexico'
+)
+
+INSERT INTO Customers Values
+(
+	'Antonio Moreno Taquería',
+	'Antonio Moreno',
+	'Mataderos 2312',
+	'México D.F.',
+	05023,
+	'Mexico'
+)
+
+INSERT INTO Customers Values
+(
+	'Around the Horn',
+	'Thomas Hardy',
+	'120 Hanover Sq.',
+	'London',
+	'WA1 1DP',
+	'UK'
+)
+
+INSERT INTO Customers Values
+(
+	'Berglunds snabbköp',
+	'Christina Berglund',
+	'Berguvsvägen 8',
+	'Luleå',
+	'S-958 22',
+	'Sweden'
+)
+-- Data Entry Ends Here --
