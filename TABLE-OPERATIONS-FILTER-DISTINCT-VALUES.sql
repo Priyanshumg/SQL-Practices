cd6436d5002c48327f7ee2565703ab0b5d716ea6
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-02-20 11:12:27 +0530

    [Priyanshu] Add: Added New Table to Database

--- TABLE-OPERATIONS-FILTER-DISTINCT-VALUES.sql
+++ TABLE-OPERATIONS-FILTER-DISTINCT-VALUES.sql
@@ -1,3 +1,16 @@
 -- Created a new Database for Practice
 CREATE DATABASE Practice
 
+-- Using Practice DataBase
+USE Practice 
+
+CREATE TABLE Customers
+(
+	CustomerID int Identity (1,1) Not Null,
+	CustomerName nvarchar(50) Not Null,
+	ContactName nvarchar(50) Not Null,
+	"Address" nvarchar(50) Not Null,
+	City nvarchar(50) Not Null,
+	PostalCode int Not Null,
+	Country nvarchar(50) Not Null,
+)
