164b7e6a72a2bdb14ba6cb38330f3ef0104e05c4
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-02-21 21:30:53 +0530

    [Priyanshu] DEL: Deleted the Key constraint

--- TABLE-ADD-UNIQUE-KEY.sql
+++ TABLE-ADD-UNIQUE-KEY.sql
@@ -12,3 +12,6 @@ ADD CONSTRAINT UK_TBLPerson_Email UNIQUE(Email)
 -- Trying adding same mail to check UNQIUE KEYS
 INSERT INTO TBLPerson VALUES (1,'Rob','r@r.com', 2, 99)
 
+-- Deleting UNIQUE Key
+ALTER TABLE TBLPerson
+DROP CONSTRAINT UK_TBLPerson_Email
