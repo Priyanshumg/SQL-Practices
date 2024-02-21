745fb583c120100afa2f66acb115c435ace6215d
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-02-21 21:30:05 +0530

    [Priyanshu] Add: Added a small testing to the database

--- TABLE-ADD-UNIQUE-KEY.sql
+++ TABLE-ADD-UNIQUE-KEY.sql
@@ -9,3 +9,6 @@ SELECT * FROM TBLPerson
 ALTER TABLE TBLPerson
 ADD CONSTRAINT UK_TBLPerson_Email UNIQUE(Email)
 
+-- Trying adding same mail to check UNQIUE KEYS
+INSERT INTO TBLPerson VALUES (1,'Rob','r@r.com', 2, 99)
+
